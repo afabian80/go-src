b02d1df9526f8d16656c8ad8a9b63dba2692c950
Akos Fabian <fabian.akos@gmail.com>
2018-01-27 14:17:18 +0100

    Printing but a bit untidy

--- fat-to-track-list/main.go
+++ fat-to-track-list/main.go
@@ -1,21 +1,50 @@
 package main
 
+/*
+My first line filter Go program to enumerate tracks
+on a FAT formatted pendrive.
+The input if from the command "sudo fatsort -fl /dev/sdc1",
+output is stdout
+*/
+
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
+	fileIndex := 0
+	folderIndex := 0
+	isStarted := false
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		folderMatch, _ := regexp.MatchString("^/\\w+.*", line)
+		if folderMatch {
+			isStarted = true
+			folderIndex++
+			fmt.Printf("\n\nAlbum %d - %v\n\n", folderIndex, fixAlbumName(line))
+		}
+		fileMatch, _ := regexp.MatchString("^[^/]+", line)
+		if fileMatch {
+			if isStarted {
+				fileIndex++
+				fmt.Printf("\t%5d: %v\n", fileIndex, line)
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println(os.Stderr, "error: ", err)
 		os.Exit(1)
 	}
+}
+
+func fixAlbumName(in string) string {
+	return strings.Replace(strings.Title(in), "/", "", -1)
 }
